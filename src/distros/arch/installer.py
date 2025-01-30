ad474df9c3776a8b5f4821cf9df85d370f8d2083
ochibani <11yzyv86j@relay.firefox.com>
2025-01-30 17:54:53 +0000

    update ash-installer

--- src/distros/arch/installer.py
+++ src/distros/arch/installer.py
@@ -60,7 +60,7 @@ def main():
     os.system("/sbin/hwclock --systohc")
     os.system("useradd -m -s /bin/bash aur")
     os.system("echo 'aur ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers")
-    install_ash = os.system("su aur -c 'paru -S ash-git --noconfirm'")
+    install_ash = os.system("pacman -U /mnt/var/cache/pacman/pkg/ash-git.pkg.tar.zst --noconfirm")
     if install_ash != 0:
         sys.exit(1)
 
